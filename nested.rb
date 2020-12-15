d706a5434cd135a0247b7f1f534abae0f408b7a7
Romano Livi <romano_livi@outlook.com>
2020-12-15 17:10:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,13 +15,13 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+     
+     return programmer_hash[:grace_hopper]
 end
 
+
+
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+    return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+    return programmer_hash[:dennis_ritchie][:languages].join(" ")
 end
 
 def adding_matz
@@ -80,6 +84,10 @@ def adding_matz
         }
      }
 
+return programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+return programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+
+
     
 end
 
@@ -101,7 +109,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+    return programmer_hash[:alan_kay][:known_for] = "GUI"
      
 end
 
@@ -121,5 +129,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+    return programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
 
 end
